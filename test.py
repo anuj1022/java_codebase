e6f765eb0c8cfa215250486940e609c1b217c395
anuj1022 <132140437+anuj1022@users.noreply.github.com>
2023-04-29 17:21:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+data={"name":"ineuron","course":["data science","cloud","app development","graphic design"],"greeting":"greeting fron ineuron"}
+def get_course():
+    return data['course']
+def greeting():
+    return data['greeting']
+greeting()
